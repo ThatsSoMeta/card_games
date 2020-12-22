db3329a811ff8c5324a76bd89bdf81dd803d2807
Drew Sexton <acsexton@gmail.com>
2020-12-22 15:27:45 -0500

    moved trump assignment to its own function, rearranged some things in the logic

--- euchre.py
+++ euchre.py
@@ -2,10 +2,10 @@ from main import Player, Deck
 import random
 import os
 
-Drew = Player('Drew')
-Mom = Player('Mom')
-Erin = Player('Erin')
-Brandi = Player('Brandi')
+# Drew = Player('Drew')
+# Mom = Player('Mom')
+# Erin = Player('Erin')
+# Brandi = Player('Brandi')
 
 
 def next_player(current_player, players):
@@ -21,49 +21,74 @@ def euchre():
     Play a game of euchre! (kinda)
     NOTE: There will not be this many prints in the real code.
     """
+    os.system('cls' if os.name == 'nt' else 'clear')
     deck = Deck()
     deck.deck = [card for card in deck.deck if card.value >= 9]  # only use 9+
     deck.shuffle()
-    game_active = True
-    trump = None
-    alone = False
-    possible_trump = [
-        {'suit': 'hearts', 'color': 'red'},
-        {'suit': 'diamonds', 'color': 'red'},
-        {'suit': 'clubs', 'color': 'black'},
-        {'suit': 'spades', 'color': 'black'},
-                    ]
-    team1 = {"score": 0, "players": [Drew, Mom]}
-    team2 = {"score": 0, "players": [Erin, Brandi]}
+    players = []
+    team1 = {"score": 0, "players": []}
+    team2 = {"score": 0, "players": []}
     # code below assigns teams via terminal prompt
     # this will be simplified in the actual app
-    # for i in range(2):
-    #     for j in range(2):
-    #         player = Player(input(f"Team {i + 1}, Player {j + 1} name: "))
-    #         if i == 0:
-    #             team1["players"].append(player)
-    #         else:
-    #             team2["players"].append(player)
-    players = []
+    for i in range(2):
+        for j in range(2):
+            player = Player(input(f"Team {i + 1}, Player {j + 1} name: "))
+            if i == 0:
+                team1["players"].append(player)
+            else:
+                team2["players"].append(player)
     for i in range(2):
         players.append(team1["players"][i])
         players.append(team2["players"][i])
+    # alone = False
+    # game_active = True
     # Gameplay logic starts below. Will likely split up into multiple functions
+    # LET THE GAMES BEGIN!
     dealer = random.choice(players)
     current_player = dealer
-    print(f"Current Player: {current_player}")
     print('Here we go!!')
     print(f"Players: {[player.name for player in players]}")
     print(f"Dealer: {dealer}")
     for _ in range(5):
         for player in players:
             player.deal(deck.deal())
+    trump = select_trump(players, dealer, current_player, deck)
+    current_player = next_player(dealer, players)
+    # while game_active:
+    print(f"{dealer} is dealer. Game will begin with {current_player}.")
+    for _ in range(5):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        trick = []
+        for player in players:
+            trick.append(euchre_play(current_player, trick, players, trump))
+            print(f"{current_player}'s turn ends.")
+            current_player = next_player(current_player, players)
+            os.system('cls' if os.name == 'nt' else 'clear')
+        trick_values = [hand[1].value for hand in trick]
+        for hand in trick:
+            if hand[1].value == max(trick_values):
+                winner = hand
+        print(f"Winning card: {winner}")
+        winning_team = team1 if winner[0] in team1 else team2
+        print(f"{winning_team['players'][0]} and {winning_team['players'][1]} take the trick.")
+
+
+def select_trump(players, dealer, current_player, deck):
+    possible_trump = [
+        {'suit': 'hearts', 'color': 'red'},
+        {'suit': 'diamonds', 'color': 'red'},
+        {'suit': 'clubs', 'color': 'black'},
+        {'suit': 'spades', 'color': 'black'},
+                    ]
+    trump = None
     turn_up = deck.deck[0]
     print(f"Turn up: {turn_up.name} of {turn_up.suit}")
     current_player = next_player(dealer, players)
     print(f"{current_player}'s turn!")
-    for i in range(4):
+    for _ in range(4):
         if not trump:
+            print(f"Players: {players}")
+            input(f"Press any key when {current_player.name} has the computer...")
             print()
             print(f"{current_player.name}'s hand: {current_player.hand}")
             print(f"""\tWould you like {dealer} to pick up the {turn_up.name} of {turn_up.suit}?""")
@@ -78,12 +103,15 @@ def euchre():
                 print(f"{current_player} ends turn. {next_player(current_player, players)} is next.")
                 current_player = next_player(current_player, players)
                 print(f"{current_player}'s turn.")
+        os.system('cls' if os.name == 'nt' else 'clear')
     for option in possible_trump:
         if option['suit'] == turn_up.suit:
             possible_trump.pop(possible_trump.index(option))
     options = [option['suit'] for option in possible_trump]
-    for i in range(4):
+    for _ in range(4):
+        os.system('cls' if os.name == 'nt' else 'clear')
         if not trump:
+            input(f'Press any key when {current_player.name} has the computer...')
             print()
             print(f"{current_player.name}'s hand: {current_player.hand}")
             print(f"{current_player.name}, what suit would you like to call (if any)?")
@@ -108,33 +136,16 @@ def euchre():
                 card.value += 20
         elif card.suit == trump['suit']:
             card.value += 13
-    # LET THE GAMES BEGIN!
-    current_player = next_player(dealer, players)
-    # while game_active:
-    print(f"{dealer} is dealer. Game will begin with {current_player}")
-    for _ in range(5):
-        os.system('cls' if os.name == 'nt' else 'clear')
-        trick = []
-        for player in players:
-            trick.append(euchre_play(current_player, trick, players, trump))
-            print(f"{current_player}'s turn ends.")
-            current_player = next_player(current_player, players)
-            os.system('cls' if os.name == 'nt' else 'clear')
-        trick_values = [hand[1].value for hand in trick]
-        for hand in trick:
-            if hand[1].value == max(trick_values):
-                winner = hand
-        print(f"Winning card: {winner}")
-        winning_team = team1 if winner[0] in team1 else team2
-        winning_team['score'] += 1
-        print(f"Winning team: {winning_team}")
+    return trump
 
 
 def euchre_play(player, trick, players, trump):
-    input(f"Press Enter when {player} has the computer...")
+    input(f"Press any key when {player} has the computer...")
     hand = player.hand
     suits_in_hand = [card.suit for card in player.hand]
     hand.sort(key=lambda x: x.value, reverse=True)
+    print(f"Players: {players}")
+    print(f"Dealer: {dealer}")
     print(f"It is {player}'s turn...")
     print(f"Trump is {trump['suit']}")
     print(f"On the table: {trick}")
@@ -143,7 +154,7 @@ def euchre_play(player, trick, players, trump):
             print(f"{i} - {card.name} of {card.suit}")
         play = input('Which card would you like to play? ')
         while not play.isdigit() or int(play) not in range(len(hand)):
-            play = input('Please pick a number from the given list...')
+            play = input('Please pick a number from the given list... ')
         play = int(play)
         print(f"{player} plays {hand[play]}")
         return (player, hand.pop(play))
@@ -163,10 +174,18 @@ def euchre_play(player, trick, players, trump):
                 print(f"You must follow suit by playing {lead_suit}...")
                 play = None
                 print(f"{trump['suit'].title()} is trump.")
-            else:
-                pass
         print(f"{player} plays {hand[play].name} of {hand[play].suit}")
         return (player, hand.pop(play))
+
+
+def check_winner(teams):
+    scores = [team['score'] for team in teams]
+    if max(scores) - min(scores) < 2:
+        return False
+    for team in teams:
+        if team['score'] == max(scores):
+            print(f"{team['players'][0]} and {team['players'][1]} win!!")
+            return team
 
 
 if __name__ == "__main__":
